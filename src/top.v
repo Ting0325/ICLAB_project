f76c7cb2b29ca529349029143c36ad80c49749ba
Ting0325 <41935725+Ting0325@users.noreply.github.com>
2020-01-02 03:27:06 +0800

    Add files via upload

--- src/top.v
+++ src/top.v
@@ -13,6 +13,10 @@ wire [4:0] rs1,rs2;
 wire [4:0] rd;
 wire [11:0] imm;
 wire [4:0] rs1_data,rs2_data;
+//commit bus
+wire [5:0] commit_idx;
+wire [31:0]commit_data;
+wire commit_wen;
 //common data bus
 wire  ADD1_valid;
 wire [31:0] ADD1_result;
@@ -24,6 +28,9 @@ wire  MUL1_valid;
 wire [31:0] MUL1_result;
 wire  MUL2_valid;
 wire [31:0] MUL2_result;
+wire LS_valid;
+wire [31:0]LS_value;
+wire [2:0]LS_idx;
 //execution unit inputs
 wire [31:0] ADD1_Vj;
 wire [31:0] ADD1_Vk;
@@ -40,16 +47,40 @@ wire [31:0] MULT1_op;
 wire [31:0] MULT2_Vj;
 wire [31:0] MULT2_Vk;
 wire [31:0] MULT2_op;
+//d_cache
+wire [31:0] dina;
+wire [18:0] addra, addrb;
+wire wea; 
+//order manager
+wire [2:0] ls_entry;
+wire ls_full;
+wire busy_add1, busy_add2, busy_add3;
+wire busy_mul1, busy_mul2;
+wire struct_haz;
+wire [3:0] rs_idx;
+wire [3:0] Qj, Qk; //renamed value
+
+
 
 wire operation;
+
+//pc
+always@(posedge clk) begin
+	if((struct_haz)||(~start))
+		pc <= pc;
+	else 
+		pc <= pc+4;
+end
+
+
 //i-cache
 cache i_cache(
   .dina(i_instruction),       //data to be written
-  .addrb(pc),  //address for read operation
-  .addra(i_addr), //address for write operation
-  .wea(i_wea),         //write enable signal
-  .clk(clk),  //clock signal for write operation
-  .doutb(instruction)           //read data
+  .addrb(pc),  							 //address for read operation
+  .addra(i_addr), 					 //address for write operation
+  .wea(i_wea),         			 //write enable signal
+  .clk(clk),  							 //clock signal for write operation
+  .doutb(instruction)         //read data
 );
 //decoder
 decoder decoder0(
@@ -71,9 +102,9 @@ regfile regfile0(
 	.read_addr2(rs2),
 	.read_data2(rs2_data),
 // Port Write
-	.write_addr(),
-	.write_data(),
-	.write()//write enable
+	.write_addr(commit_idx),
+	.write_data(commit_data),
+	.write(commit_wen)//write enable
 );
 //order manager
 	//renaming table
@@ -83,24 +114,38 @@ order_manager order_manager(
 	.rst_n(rst_n),
 	.instruction(instruction),
     //busy info from reservation stations
-	.ls_entry(),
-	.ls_full(),
-	.busy_add1(),
-	.busy_add2(),
-	.busy_add3(),
-	.busy_mul1(),
-	.busy_mul1(),
-	.cdb(),
-	.commit_idx(),
-	.commit_data(),
-	.commit_wen(),
-	.Qj(),
-	.Qk(),
-    .rs_idx(),
-    .struct_haz()
+	.ls_entry(ls_entry),
+	.ls_full(ls_full),
+	.busy_add1(busy_add1),
+	.busy_add2(busy_add2),
+	.busy_add3(busy_add3),
+	.busy_mul1(busy_mul1),
+	.busy_mul2(busy_mul2),
+	.commit_idx(commit_idx),
+	.commit_data(commit_data),
+	.commit_wen(commit_wen),
+	//common data bus
+	.ADD1_valid(ADD1_valid),
+	.ADD1_result(ADD1_result),
+	.ADD2_valid(ADD2_valid),
+	.ADD2_result(ADD2_result),
+	.ADD3_valid(ADD3_valid),
+	.ADD3_result(ADD3_result),
+	.MULT1_valid(MULT1_valid),
+	.MULT1_result(MULT1_result),
+	.MULT2_valid(MULT2_valid),
+	.MULT2_result(MULT2_result),
+	.LS_valid(LS_valid),
+	.LS_value(LS_value),
+	.LS_idx(LS_idx),
+	
+	.Qj(Qj),
+	.Qk(Qk),
+  .rs_idx(rs_idx),
+  .struct_haz(struct_haz)
     
     );
-    //
+    
 //reservation stations
 RS RS0(
 	.clk(clk),
@@ -109,22 +154,22 @@ RS RS0(
 	.s1(rs1_data),//value from register file
 	.s2(rs2_data),
 //rename
-	.Qj(),
-	.Qk(),
+	.Qj(Qj),
+	.Qk(Qk),
 //common data bus
-	.ADD1_valid(),
-	.ADD1_result(),
-	.ADD2_valid(),
-	.ADD2_result(),
-	.ADD3_valid(),
-	.ADD3_result(),
-	.MULT1_valid(),
-	.MULT1_result(),
-	.MULT1_valid(),
-	.MULT2_result(),
-	.LS_valid(),
-	.LS_value(),
-	.LS_idx(),
+	.ADD1_valid(ADD1_valid),
+	.ADD1_result(ADD1_result),
+	.ADD2_valid(ADD2_valid),
+	.ADD2_result(ADD2_result),
+	.ADD3_valid(ADD3_valid),
+	.ADD3_result(ADD3_result),
+	.MULT1_valid(MULT1_valid),
+	.MULT1_result(MULT1_result),
+	.MULT2_valid(MULT2_valid),
+	.MULT2_result(MULT2_result),
+	.LS_valid(LS_valid),
+	.LS_value(LS_value),
+	.LS_idx(LS_idx),
 //outputs to EXE
 	.ADD1_Vj(ADD1_Vj),
 	.ADD1_Vk(ADD1_Vk),
@@ -143,20 +188,9 @@ RS RS0(
 	.MULT2_Vk(MULT2_Vk),
 	.MULT2_Op(MULT2_op),
 
-/*	.LOAD1_addr(),
-	.LOAD2_addr(),
-	.LOAD3_addr(),
-
-	.STORE1_addr(),
-	.STORE1_Qi(),
-	.STORE2_addr(),
-	.STORE2_Qi(),
-	.STORE3_addr(),
-	.STORE3_Qi()
-*/
-.LS_addr()
-.LS_data()
-.LS_wen()
+	.LS_addr(addr_dcache)
+	.LS_data(dina)
+	.LS_wen(wea)
 );
 
 //execution unit
@@ -168,7 +202,7 @@ EXE_add EXE_ADD1(
 	.Op(ADD1_op),
 	.a(ADD1_Vj),
 	.b(ADD1_Vk),
-	.valid(),
+	.valid(ADD1_valid),
 	.result(ADD1_result)
 );
 
@@ -179,7 +213,7 @@ EXE_add EXE_ADD2(
     .Op(ADD2_op),
     .a(ADD2_Vj),
     .b(ADD2_Vk),
-    .valid(),
+    .valid(ADD2_valid),
     .result(ADD2_result)
 );
 
@@ -190,7 +224,7 @@ EXE_add EXE_ADD3(
     .Op(ADD3_op),
     .a(ADD3_Vj),
     .b(ADD3_Vk),
-    .valid(),
+    .valid(ADD3_valid),
     .result(ADD3_result)
 );
 
@@ -202,7 +236,7 @@ EXE_mul EXE_MUL1(
 	.Op(MUL1_op),
 	.a(MUL1_Vj),
 	.b(MUL1_Vk),
-	.valid(),
+	.valid(MUL1_valid),
 	.result(MUL1_result)
 );
 
@@ -213,19 +247,19 @@ EXE_mul EXE_MUL2(
     .Op(MUL2_op),
     .a(MUL2_Vj),
     .b(MUL2_Vk),
-    .valid(),
+    .valid(MUl2_valid),
     .result(MUL2_result)
 );
 
 
 //d-cache
 cache d_cache(
-	.dina(),       //data to be written
-	.addrb(),  //address for read operation
-	.addra(), //address for write operation
-	.wea(),         //write enable signal
-	.clk(),  //clock signal for write operation
-	.doutb()           //read data
+	.dina(dina),       			 //data to be written
+	.addrb(addr_dcache),  		 //address for read operation
+	.addra(addr_dcache),     //address for write operation
+	.wea(wea),       //write enable signal
+	.clk(clk),       //clock signal for write operation
+	.doutb(LS_value)      //read data
 );
 
 endmodule
